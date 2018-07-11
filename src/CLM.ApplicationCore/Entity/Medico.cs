46c6ed11037f9798671dc42d2387dbab50eacfee
Marcelo Silva <desenv.marcelo.silva@gmail.com>
2018-07-10 21:31:57 -0300

    Add project files.

---
+++ src/CLM.ApplicationCore/Entity/Medico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CLM.ApplicationCore.Entity
+{
+    public class Medico
+    {
+		public Medico()
+		{
+
+		}
+
+		public int MedicoId { get; set; }
+		public string Nome { get; set; }
+		public string CRM { get; set; }
+	}
+}
---
+++ src/CLM.Infrastructure/Data/CLMContext.cs
@@ -0,0 +1,26 @@
+﻿using CLM.ApplicationCore.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CLM.Infrastructure.Data
+{
+    public class CLMContext : DbContext
+    {
+		public CLMContext(DbContextOptions<CLMContext> options) 
+			: base(options)
+		{
+			
+		}
+
+		public DbSet<Medico> Medicos { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			// Configura o entity
+			modelBuilder.Entity<Medico>().ToTable("Medico");
+			//base.OnModelCreating(modelBuilder);
+		}
+	}
+}
