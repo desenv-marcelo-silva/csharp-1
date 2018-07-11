d21a869a25735af0bdb192925c6ba4b6dd9c697c
Marcelo Silva <desenv.marcelo.silva@gmail.com>
2018-07-10 21:51:04 -0300

    Inclusão da classe paciente

---
+++ src/CLM.ApplicationCore/Entity/Paciente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CLM.ApplicationCore.Entity
+{
+    public class Paciente
+    {
+		public Paciente()
+		{
+
+		}
+
+		public int PacienteId { get; set; }
+		public string Nome { get; set; }
+		public string  email { get; set; }
+		public string Telefone { get; set; }
+
+	}
+}
