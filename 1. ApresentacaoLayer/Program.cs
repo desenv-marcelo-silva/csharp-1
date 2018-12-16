b69ebd793f489359288f33610489923923388e96
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2018-12-16 11:24:43 -0200

    Ajustes no projeto inicial.

--- 1. ApresentacaoLayer/Program.cs
+++ 1. ApresentacaoLayer/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using DataLayer.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace _1._ApresentacaoLayer
@@ -14,7 +11,24 @@ namespace _1._ApresentacaoLayer
 	{
 		public static void Main(string[] args)
 		{
-			CreateWebHostBuilder(args).Build().Run();
+			IWebHost host = CreateWebHostBuilder(args).Build();
+			using (IServiceScope scope = host.Services.CreateScope())
+			{
+				var services = scope.ServiceProvider;
+
+				try
+				{
+					var context = services.GetRequiredService<DBContexto>();
+					DbInitializer.Initialize(context);
+				}
+				catch (Exception ex)
+				{
+					var logger = services.GetRequiredService<ILogger<Program>>();
+					logger.LogError(ex, "Erro ao preencher dados padrão no database.");
+				}
+			}
+
+			host.Run();
 		}
 
 		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
